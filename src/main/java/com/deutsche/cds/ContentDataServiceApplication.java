52f89851ee301133dda466d56f3acb5c6a21d1d1
Nilesh <nileshphatak4@gmail.com>
2026-05-20 22:37:52 +0530

    initial commit

---
+++ src/main/java/com/deutsche/cds/ContentDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.deutsche.cds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentDataServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deutsche/cds/ContentDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deutsche.cds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
